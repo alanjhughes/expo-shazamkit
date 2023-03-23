d28e5b00e886ac2d31627c05a41ed998dfea89e4
Alan Hughes <hughesno1@gmail.com>
2023-03-23 21:59:56 +0000

    add plugin

---
+++ plugin/src/withShazamKit.ts
@@ -0,0 +1,20 @@
+import { ConfigPlugin, createRunOncePlugin } from "expo/config-plugins";
+
+const pkg = require("expo-shazamkit/package.json");
+
+const MICROPHONE_USAGE = "Allow $(PRODUCT_NAME) to access your microphone";
+
+const withShazamKit: ConfigPlugin<{
+  microphonePermission?: string;
+}> = (config, { microphonePermission } = {}) => {
+  if (!config.ios) config.ios = {};
+  if (!config.ios.infoPlist) config.ios.infoPlist = {};
+  config.ios.infoPlist.NSMicrophoneUsageDescription =
+    microphonePermission ||
+    config.ios.infoPlist.NSMicrophoneUsageDescription ||
+    MICROPHONE_USAGE;
+
+  return config;
+};
+
+export default createRunOncePlugin(withShazamKit, pkg.name, pkg.version);
